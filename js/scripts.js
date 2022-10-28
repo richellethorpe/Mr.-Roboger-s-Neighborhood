8ba988570e865e9fd9d021684d881bca73eb0e12
Richelle Thorpe <richelle.ak@gmail.com>
2022-10-28 09:29:17 -0700

    update UI logic to reveal user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 //UI logic
+window.onload = function() {
+  let form = document.querySelector("form");
+  form.onsubmit = function(event) {
+    event.preventDefault();
 
+    const userInputValue = document.getElementById("userInput").value;
+  
+    document.querySelector("p#resultValues").innerText = userInputValue;
+    document.querySelector("div#results").removeAttribute("class");
+  }
+}
 
 
 
