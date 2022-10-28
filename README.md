0db27e943fc4149d0dee9d63cf8106ce4d9d9ff3
Richelle Thorpe <richelle.ak@gmail.com>
2022-10-28 11:10:57 -0700

    add UI logic to display array of values based upon used input

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 Describe: wordSubstitute()
 
+Test: "It should take the string entered by user and turn it into an array"
+Code:
+const text = "10";
+Expected Output: =["10"]
+
+Test: "It should create array of ingeters from 1 up to the number entered by user"
+Code:
+const text = "userInput";
+wordCounter(text);
+Expected Output: =[1,2,3,4]
+
 Test: "It should return ingeters from 1 up to the number entered by user"
 Code:
 const text = "userInput";
--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,29 @@ window.onload = function() {
 
     const userInputValue = document.getElementById("userInput").value;
   
-    document.querySelector("p#resultValues").innerText = userInputValue;
+    document.querySelector("p#resultValues").innerText = resultArray;
     document.querySelector("div#results").removeAttribute("class");
   }
 }
 
 //Business Logic
 
+//const resultArray = userInputValue.split();
+
+function countNumbers (countTo) {
+ 
+    const resultArray = [];
+    for (let i = 0; i <= countTo; i += countBy) {
+      resultArray.push(i);
+    }
+    return resultArray;
+  }
+
+//function countUp (countTo) {
+   //let resultArrayNew = [];
+   //for(let i = 1; i<=userInputValue; i+= 1) {
+   // resultArray.push(i);
+    
+ //  }
+  // return resultArray
+//}
