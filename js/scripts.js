a2e98565867f6546da94702dcf4bdb3cc74c298c
Richelle Thorpe <richelle.ak@gmail.com>
2022-10-28 16:03:11 -0700

    add for each functionality

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ function getResult(event) {
   event.preventDefault();
 
     const userInputValue = document.getElementById("userInput").value;
-    const resultArray= countNumbers(parseInt(userInputValue));
+    const resultArray= countNumbers(userInputValue);
     const p =document.querySelector("p#resultValues");
     document.querySelector("p#resultValues").innerText = resultArray;
     document.querySelector("div#results").removeAttribute("class");
@@ -19,11 +19,36 @@ function getResult(event) {
 
 //const resultArray = userInputValue.split();
 
-function countNumbers (countTo) {
-  const resultArray = [];
-  for (let i = 0; i <= countTo; i += 1) {
-    resultArray.push(i);
+function countNumbers (inputNum) {
+  const userNumbersArray = [];
+  let resultArray = [];
+  for (let i = 0; i <= inputNum; i += 1) {
+    userNumbersArray.push(i.toString());
+    
   }
-  return resultArray;
+  userNumbersArray.forEach( function (element){
+    if(element.includes("3")){
+    resultArray.push("Won't you be my neighbor?");
+    }else if (element.includes("2")){
+    resultArray.push("Beep!");
+    }else if (element.includes("1")){
+    resultArray.push("Boop!");
+    }else {
+    resultArray.push(element)
+  }
+  })
+
+  console.log(userNumbersArray, resultArray);
+  return userNumbersArray;
+
 }
 
+
+function replaceWords(imputNum){
+  
+  resultArrayString.forEach(function (element) {
+    if (element.includes (1))
+    return element.replace(element, "beep")
+  })
+}
+//
